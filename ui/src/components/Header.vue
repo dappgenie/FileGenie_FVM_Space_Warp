fc017ab6b1161e6648ed9cc3e3f84f9c4a91719c
mekhakrishnanm <mekhakrishnanm@gmail.com>
2023-02-05 02:53:07 +0530

    Folder List Added

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -32,7 +32,7 @@ const isOpen = ref<boolean>(false)
 }
 
 .navbar {
-  @apply h-20 shadow-lg w-full flex justify-end items-center py-5 px-4 md:px-10 xl:px-12 2xl:px-14;
+  @apply h-20 shadow-lg w-full flex items-center py-5 px-4 md:px-10 xl:px-12 2xl:px-14;
 }
 
 .mob-nav {
